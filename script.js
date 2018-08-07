1196e165af3257b9f628f0f5f96ae5334ec7a100
Bryan Anderson <bryan.texasbobcat@gmail.com>
2018-08-07 16:15:06 -0500

    Lul

--- script.js
+++ script.js
@@ -1,1 +1,60 @@
 // Your JS goes here
+console.log('booyah');
+
+window.onload = function checkerboard () {
+
+    function createSquares () {
+    const blackSquares = document.createElement('div');
+    blackSquares.style.backgroundColor= 'black';
+    blackSquares.style.width = '11.1%';
+    blackSquares.style.cssFloat = 'left';
+    blackSquares.style.paddingBottom = '11.1%';
+    blackSquares.className = 'Black';
+    document.body.appendChild(blackSquares);
+
+    const redSquares = document.createElement('div');
+    redSquares.style.backgroundColor = 'red';
+    redSquares.style.width = '11.1%';
+    redSquares.style.cssFloat = 'left';
+    redSquares.style.paddingBottom = '11.1%';
+    redSquares.className = 'Red';
+    document.body.appendChild(redSquares);
+    }
+
+    createSquares();
+    createSquares();
+    createSquares();
+    createSquares();
+    createSquares();
+    createSquares();
+    createSquares();
+    createSquares();
+    createSquares();
+    createSquares();
+    createSquares();
+    createSquares();
+    createSquares();
+    createSquares();
+    createSquares();
+    createSquares();
+    createSquares();
+    createSquares();
+    createSquares();
+    createSquares();
+    createSquares();
+    createSquares();
+    createSquares();
+    createSquares();
+    createSquares();
+    createSquares();
+    createSquares();
+    createSquares();
+    createSquares();
+    createSquares();
+    createSquares();
+    createSquares();
+    createSquares();
+    createSquares();
+    createSquares();
+    createSquares();
+}
